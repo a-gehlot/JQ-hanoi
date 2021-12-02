ecafb2b443844e9065b04c0d3473f5102d9d4e32
a-gehlot <andrew.gehlot@gmail.com>
2021-12-02 14:28:17 -0500

    Naive game representation elements

--- dist/main.js
+++ dist/main.js
@@ -25,7 +25,7 @@ eval("class Game {\n  constructor() {\n    this.towers = [[3, 2, 1], [], []];\n 
   \***************************/
 /***/ ((module) => {
 
-eval("class View {\n    constructor(hanoiGame, $element) {\n        this.hanoiGame = hanoiGame;\n        this.$element = $element;\n    }\n}\n\nmodule.exports = View;\n\n//# sourceURL=webpack:///./src/hanoi-view.js?");
+eval("class View {\n    constructor(hanoiGame, $element) {\n        this.hanoiGame = hanoiGame;\n        this.$element = $element;\n        this.setupTowers();\n    }\n\n    setupTowers() {\n        this.$element.append(`<ul class=\"tower-one\">\n        <li class=\"large\"></li>\n        <li class=\"medium\"></li>\n        <li class=\"small\"></li>\n        </ul>\n        <ul class=\"tower-two\"></ul>\n        <ul class=\"tower-three\"></ul>\n        `)\n    }\n\n}\n\nmodule.exports = View;\n\n//# sourceURL=webpack:///./src/hanoi-view.js?");
 
 /***/ }),
 
--- src/hanoi-view.js
+++ src/hanoi-view.js
@@ -2,7 +2,20 @@ class View {
     constructor(hanoiGame, $element) {
         this.hanoiGame = hanoiGame;
         this.$element = $element;
+        this.setupTowers();
     }
+
+    setupTowers() {
+        this.$element.append(`<ul class="tower-one">
+        <li class="large"></li>
+        <li class="medium"></li>
+        <li class="small"></li>
+        </ul>
+        <ul class="tower-two"></ul>
+        <ul class="tower-three"></ul>
+        `)
+    }
+
 }
 
 module.exports = View;
